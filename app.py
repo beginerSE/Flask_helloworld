21a929fec5410f74962503865e8e2fc9c221c19c
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-07 11:21:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 def hello_World():
     return 'HelloWorld！'
 
+# /helloにアクセスするとsample.htmlの中身が表示される
+@app.route('/hello')
+def hello():
+    return render_template("sample.html")
 
-
-# サーバー起動
+# デバックモードでアプリを起動
 app.run(debug=True)
